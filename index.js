45b52da5cf6d96253410ddb605cb5470e9f93ad0
Toji Takuro <takuro.toji@gmail.com>
2022-02-06 05:05:40 +0900

    クエスト受注ボタンを追加

--- index.js
+++ index.js
@@ -11,17 +11,29 @@ pingButton.addEventListener('click', async () => {
 
 const questBoard = document.getElementById('questBoard');
 
+const postQuest = name => {
+  console.log(`Posted: ${name}`);
+};
+
 let count = 1;
 const addQuestButton = document.getElementById('addQuestButton');
 addQuestButton.addEventListener('click', async () => {
+  const title = `Quest ${count}`;
+  const discription = `Discription about quest ${count}`;
+
   // クエストを追加する処理
   const t = document.getElementById('questTemplate');
-  t.content.querySelector('div#questTitle').textContent = `Quest ${count}`;
-  t.content.querySelector(
-    'div#questDiscription'
-  ).textContent = `Discription about quest ${count}`;
+  t.content.querySelector('div#questTitle').textContent = title;
+  t.content.querySelector('div#questDiscription').textContent = discription;
 
   const questPaper = document.importNode(t.content, true);
+  // 実体化してからイベント登録する必要がある
+  const postQuestButton = questPaper.getElementById('postQuestButton');
+  postQuestButton.addEventListener('click', () => {
+    console.log('Clicked');
+    postQuest(title);
+  });
+
   questBoard.appendChild(questPaper);
 
   count += 1;
