bc4d74bf4e0755df2dab10c3822c19fecbf264d4
biyori-t <takuro.toji@gmail.com>
2022-07-11 01:00:48 +0900

    #11 preload を TypeScript 化

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,9 +1,9 @@
 const { ipcRenderer, contextBridge } = require('electron');
 
 contextBridge.exposeInMainWorld('questApi', {
-  ping: async message =>
+  ping: async (message: any) =>
     ipcRenderer.invoke('ping', { ping: `Ping: ${message}`, message }),
-  post: async quest => ipcRenderer.invoke('post', quest),
-  achieve: async achieved => ipcRenderer.invoke('achieve', achieved),
+  post: async (quest: any) => ipcRenderer.invoke('post', quest),
+  achieve: async (achieved: any) => ipcRenderer.invoke('achieve', achieved),
   report: async () => ipcRenderer.invoke('report'),
 });
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,9 @@ var main = {
         include: [path.resolve(__dirname, 'src', 'main')],
         exclude: [path.resolve(__dirname, 'node_modules')],
         loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.main.json',
+        },
       },
     ],
   },
@@ -27,29 +30,34 @@ var main = {
   },
 };
 
-var renderer = {
+var preload = {
   mode: 'development',
-  target: 'electron-renderer',
-  entry: path.join(__dirname, 'src', 'renderer', 'src', 'index'),
+  target: 'electron-preload',
+  entry: path.join(__dirname, 'src', 'main', 'preload'),
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist', 'scripts'),
+    filename: 'preload.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  resolve: {
-    extensions: ['.json', '.js', '.jsx', '.css', '.ts', '.tsx'],
+  node: {
+    __dirname: false,
+    __filename: false,
   },
   module: {
     rules: [
       {
-        test: /\.(tsx|ts)$/,
-        use: ['ts-loader'],
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, 'node_modules'),
-        ],
+        test: /.ts?$/,
+        include: [path.resolve(__dirname, 'src', 'main')],
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.preload.json',
+        },
       },
     ],
   },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
 };
 
-module.exports = [main];
+module.exports = [main, preload];
